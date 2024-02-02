fc07215c4d0bfa908c36aa5e7242d713dc0cd114
Renato Almeida <renatoam@ymail.com>
2024-02-01 23:35:37 -0300

    feat: adicionado logica do cadastro via post

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
 from models.task import Task
 
 app = Flask(__name__)
 
 tasks = []
-
+task_id_control = 1
 @app.route('/tasks',methods=['POST'])
 def create_task():
+    # sempre que for necessário fazer interações com uma variável que está fora do método
+    # é necessário usar o "global" antes do nome da variável
+    global task_id_control
     data = request.get_json()
-    print(data)
-    return 'Test'
+    # com o data.get("valor1","valor2")
+    # primeiro é verificado se na propriedade do valor1 existe valor. Se não ele assume o "valor2"
+    new_task = Task(id=task_id_control,title=data['title'],description=data.get("description",""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message":"Nova tarefa criada com sucesso"})
 
 if __name__ == '__main__':
     app.run(debug=True)
