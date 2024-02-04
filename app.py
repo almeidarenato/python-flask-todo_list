d317aed4914b7d4e31e2b65c4a1aa1ed1319038c
Renato Almeida <renatoam@ymail.com>
2024-02-03 22:25:50 -0300

    feat: adiciona break para otimizar busca e uso de recursos

--- app.py
+++ app.py
@@ -72,6 +72,7 @@ def delete_task(id):
     for t in tasks:
          if t.id == id:
              task = t
+             break
 
     if not task:
         return jsonify({"message":"Não foi possível encontrar a atividade"}),404
